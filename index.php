57413570acca9dad63b8d5adf5df321d8ee85b59
cherrywbtc <jielei.wang.0316@gmail.com>
2016-12-14 15:00:32 -0500

    shopping cart added

--- index.php
+++ index.php
@@ -1,4 +1,44 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+session_start();
+require_once("dbcontroller.php");
+$db_handle = new DBController();
+if(!empty($_GET["action"])) {
+switch($_GET["action"]) {
+	case "add":
+		if(!empty($_POST["quantity"])) {
+			$productByCode = $db_handle->runQuery("SELECT * FROM handbags WHERE code='" . $_GET["code"] . "'");
+			$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"], 'code'=>$productByCode[0]["code"], 'quantity'=>$_POST["quantity"], 'price'=>$productByCode[0]["price"]));
+			
+			if(!empty($_SESSION["cart_item"])) {
+				if(in_array($productByCode[0]["code"],$_SESSION["cart_item"])) {
+					foreach($_SESSION["cart_item"] as $k => $v) {
+							if($productByCode[0]["code"] == $k)
+								$_SESSION["cart_item"][$k]["quantity"] = $_POST["quantity"];
+					}
+				} else {
+					$_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
+				}
+			} else {
+				$_SESSION["cart_item"] = $itemArray;
+			}
+		}
+	break;
+	case "remove":
+		if(!empty($_SESSION["cart_item"])) {
+			foreach($_SESSION["cart_item"] as $k => $v) {
+					if($_GET["code"] == $k)
+						unset($_SESSION["cart_item"][$k]);				
+					if(empty($_SESSION["cart_item"]))
+						unset($_SESSION["cart_item"]);
+			}
+		}
+	break;
+	case "empty":
+		unset($_SESSION["cart_item"]);
+	break;	
+}
+}
+?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -59,57 +99,68 @@
 
 <div style="height: 47px"></div>
         <!-- content begins -->
-        		<div class="cont_top"></div>
-       			<div id="content">
-                	<div class="all_box">
-                        <div style="width: 15px; height:26px; float: left;"></div>
-                        	<div class="home_box">
-                          		<a href="item1.html"><img src="images/one.jpg" class="img_l" alt="" /></a>
-                        	</div>
-                            <div style="width: 15px; height:26px; float: left;"></div>
-                            
-                        	<div class="home_box">
-                         		<img src="images/two.jpg" class="img_l" alt="" />
-                                <div style="height:20px"><a href="#" class="read">read more</a></div>
-                            </div>
-                            <div style="width: 15px; height:26px; float: left;"></div>
-                            
-                        	<div class="home_box">
-                         		<img src="images/three.jpg" class="img_l" alt="" />
-                                <div style="height:20px"><a href="#" class="read">read more</a></div>
-                            </div>
-                            <div style="width: 15px; height:26px; float: left;"></div>
+    <div id="shopping-cart">
+<div class="txt-heading">Shopping Cart <a id="btnEmpty" href="index.php?action=empty">Empty Cart</a></div>
+<?php
+if(isset($_SESSION["cart_item"])){
+    $item_total = 0;
+?>	
+<table cellpadding="10" cellspacing="1">
+<tbody>
+<tr>
+<th><strong>Name</strong></th>
+<th><strong>Code</strong></th>
+<th><strong>Quantity</strong></th>
+<th><strong>Price</strong></th>
+<th><strong>Action</strong></th>
+</tr>	
+<?php		
+    foreach ($_SESSION["cart_item"] as $item){
+		?>
+				<tr>
+				<td><strong><?php echo $item["name"]; ?></strong></td>
+				<td><?php echo $item["code"]; ?></td>
+				<td><?php echo $item["quantity"]; ?></td>
+				<td align=right><?php echo "$".$item["price"]; ?></td>
+				<td><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction">Remove Item</a></td>
+				</tr>
+				<?php
+        $item_total += ($item["price"]*$item["quantity"]);
+		}
+		?>
 
-                            <div class="home_box">
-                                <img src="images/four.jpg" class="img_l" alt="" />
-                                <div style="height:20px"><a href="#" class="read">Learn more</a></div>
-                            </div>
-                            <div style="width: 15px; height:26px; float: left;"></div>
-                            
-                            <div class="home_box">
-                                <img src="images/five.jpg" class="img_l" alt="" />
-                                <div style="height:20px"><a href="#" class="read">Learn more</a></div>
-                            </div>
-                            <div style="width: 15px; height:26px; float: left;"></div>
-                            
-                            <div class="home_box">
-                                <img src="images/six.jpg" class="img_l" alt="" />
-                                <div style="height:20px"><a href="#" class="read">Learn more</a></div>
-                            </div>
-                            <div style="width: 15px; height:26px; float: left;"></div>
-                      		<div style="clear: both"></div>
-               	  	</div>
-                    <div style="height:20px"></div>
-                    <div class="box_more">
-                    	<div class="box_more_l">Donec aliquam vehicula turpis eu placerat. Nam felis sem, aliquam eget laoreet in, adipiscing eu tellus. Morbi semper suscipit turpis, sed viverra arcu ultrices vel. Curabitur congue semper mollis. Integer mauris nibh, venenatis volutpat pulvinar eu, congue id arcu. Suspendisse iaculis congue </div>
-                        <div class="box_more_r">
-                        	<a href="#" class="but_more">MORE INFO</a>
-                        </div>
-                        <div style="clear: both"></div>
-                    </div>
-                      
-                	
-        		</div>
+<tr>
+<td colspan="5" align=right><strong>Total:</strong> <?php echo "$".$item_total; ?></td>
+</tr>
+</tbody>
+</table>		
+  <?php
+}
+?>
+</div>
+
+<div id="product-grid">
+	<div class="txt-heading">Products</div>
+	<?php
+	$product_array = $db_handle->runQuery("SELECT * FROM handbags ORDER BY id ASC");
+	if (!empty($product_array)) { 
+		foreach($product_array as $key=>$value){
+	?>
+		<div class="all_box">
+			<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">
+			<div style="width: 15px; height:26px; float: left;"></div>
+			<div class="home_box"><img src="<?php echo $product_array[$key]["image"]; ?>" height = "200">
+				<div style="height:20px"><?php echo "$".$product_array[$key]["price"]; ?></div>
+				<div><input type="text" name="quantity" value="1" size="2" /><input type="submit" value="Add to cart" 					class="btnAddAction" /></div>
+			</div>
+			
+			</form>
+		</div>
+	<?php
+			}
+	}
+	?>
+</div>	
     <!-- content ends --> 
     <div style="height:40px"></div>
     <!-- bottom begin -->
